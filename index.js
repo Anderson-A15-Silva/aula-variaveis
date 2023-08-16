0c05e042c5933854c7f48f6423d4bb4031d795a0
Anderson A. Silva <andersonadsilva15@gmail.com>
2023-08-16 20:12:52 -0300

    Adicionando index.html no repositório

---
+++ index.js
@@ -0,0 +1,46 @@
+
+// Exercícios de interpretação
+/*
+Exercício 1
+Resposta: 10, 5
+*/
+
+/*
+Exercício 2
+Resposta: 10, 10, 10
+*/
+
+/*
+Exercício 3
+Resposta: Estes comandos abrem uma caixa de diálogo com o usuário, permitindo que ele coloque uma informação.
+Os nomes sugeridos são horasTrabalho e salarioDiario
+*/
+
+// Exercícios de escrita
+
+/*
+Exercício 1
+Resposta: O console não exibiu nada, provavelmente isto ocorreu porque o "programa" não entendeu a ausência de
+valores devido a eles não terem sido inseridos ao lado da váriavel (nome e idade), sendo representados
+por um elemento completamente vazio.
+
+let nome
+let idade
+
+typeof nome
+typeof idade
+*/
+
+/*
+Exercício 2
+Resposta: 
+*/
+
+const perguntaMusica = prompt("Você gosta de música? (Sim/Não)");
+console.log("Você gosta de música? (Sim/Não)");
+
+const perguntaFilme = prompt("Você tem algum filme favorito? (Sim/Não)");
+console.log("Você tem algum filme favorito? (Sim/Não)");
+
+const perguntaPais = prompt("Você nasceu no Brasil? (Sim/Não)");
+console.log("Você nasceu no Brasil? (Sim/Não)");
